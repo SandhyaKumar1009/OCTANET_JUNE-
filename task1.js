1280c07a1adb6ecf4616de449291e5e03ba58169
SandhyaKumar1009 <151362785+SandhyaKumar1009@users.noreply.github.com>
2024-06-27 21:38:28 +0530

    Add files via upload
    Task 1

---
+++ task1.js
@@ -0,0 +1,6 @@
+document.getElementById('cta-button').addEventListener('click', function() {
+    window.scrollTo({
+        top: document.getElementById('about').offsetTop,
+        behavior: 'smooth'
+    });
+});
